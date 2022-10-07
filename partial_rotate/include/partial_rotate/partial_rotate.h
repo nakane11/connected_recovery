ce3833ab6d73909b3c49ca819f7c3ca8e51ac6fd
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-10-07 20:47:47 +0900

    Add partial_rotate recovery

---
+++ partial_rotate/include/partial_rotate/partial_rotate.h
@@ -0,0 +1,83 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2009, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#ifndef PARTIAL_ROTATE_PARTIAL_ROTATE_H
+#define PARTIAL_ROTATE_PARTIAL_ROTATE_H
+#include <nav_core/recovery_behavior.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <tf2_ros/buffer.h>
+#include <base_local_planner/costmap_model.h>
+#include <string>
+
+namespace partial_rotate
+{
+/**
+ * @class PartialRotate
+ * @brief A recovery behavior that rotates the robot in-place to attempt to clear out space
+ */
+class PartialRotate : public nav_core::RecoveryBehavior
+{
+public:
+  /**
+   * @brief  Constructor, make sure to call initialize in addition to actually initialize the object
+   */
+  PartialRotate();
+
+  /**
+   * @brief  Initialization function for the PartialRotate recovery behavior
+   * @param name Namespace used in initialization
+   * @param tf (unused)
+   * @param global_costmap (unused)
+   * @param local_costmap A pointer to the local_costmap used by the navigation stack
+   */
+  void initialize(std::string name, tf2_ros::Buffer*,
+                  costmap_2d::Costmap2DROS*, costmap_2d::Costmap2DROS*);
+
+  /**
+   * @brief  Run the PartialRotate recovery behavior.
+   */
+  void runBehavior();
+
+  /**
+   * @brief  Destructor for the rotate recovery behavior
+   */
+  ~PartialRotate();
+
+private:
+  bool initialized_;
+};
+};  // namespace partial_rotate
+#endif  // PARTIAL_ROTATE_PARTIAL_ROTATE_H
---
+++ partial_rotate/src/partial_rotate.cpp
@@ -0,0 +1,97 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2009, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#include <partial_rotate/partial_rotate.h>
+#include <pluginlib/class_list_macros.h>
+// #include <nav_core/parameter_magic.h>
+// #include <tf2/utils.h>
+#include <ros/ros.h>
+// #include <geometry_msgs/Twist.h>
+// #include <geometry_msgs/Point.h>
+// #include <angles/angles.h>
+// #include <algorithm>
+// #include <string>
+#include <std_srvs/Empty.h>
+
+
+// register this planner as a RecoveryBehavior plugin
+PLUGINLIB_EXPORT_CLASS(partial_rotate::PartialRotate, nav_core::RecoveryBehavior)
+
+namespace partial_rotate
+{
+PartialRotate::PartialRotate(): initialized_(false)
+{
+}
+
+void PartialRotate::initialize(std::string name, tf2_ros::Buffer*,
+                                costmap_2d::Costmap2DROS*, costmap_2d::Costmap2DROS*)
+{
+  if (!initialized_)
+  {
+    initialized_ = true;
+  }
+  else
+  {
+    ROS_ERROR("You should not call initialize twice on this object, doing nothing");
+  }
+}
+
+PartialRotate::~PartialRotate()
+{
+}
+
+void PartialRotate::runBehavior()
+{
+  if (!initialized_)
+  {
+    ROS_ERROR("This object must be initialized before runBehavior is called");
+    return;
+  }
+
+  ROS_WARN("Rotate recovery behavior started.");
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<std_srvs::Empty>("partial_rotate");
+  std_srvs::Empty srv;
+  if (client.call(srv))
+    {
+      ROS_INFO("Succeed in calling service partial_rotate");
+    }
+  else
+    {
+      ROS_ERROR("Failed to call service partial_rotate");
+    }
+}
+};  // namespace partial_rotate
