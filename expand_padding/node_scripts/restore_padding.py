bb2a5d699e8595b8afb145dc0ae4607b3a8a1a01
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-10-26 16:09:35 +0900

    Use y-axis distance to update initial padding

--- expand_padding/node_scripts/restore_padding.py
+++ expand_padding/node_scripts/restore_padding.py
@@ -6,6 +6,7 @@ import dynamic_reconfigure.client
 
 from move_base_msgs.msg import RecoveryStatus
 from move_base_msgs.msg import MoveBaseActionResult
+from std_msgs.msg import Float32
 
 from sound_play.libsoundplay import SoundClient
 
@@ -15,11 +16,13 @@ class RestorePadding(object):
         self.shrink = False
         self.padding_name = rospy.get_param("~padding_name", "/move_base_node/global_costmap/footprint_padding")
         self.global_name = rospy.get_param("~global_name", "/move_base_node/global_costmap")
-        self.default_padding = rospy.get_param(self.padding_name)
+        # self.default_padding = rospy.get_param(self.padding_name)
         self.client = dynamic_reconfigure.client.Client(self.global_name, timeout=5.0)
         self.client_en = SoundClient(sound_action='/robotsound', blocking=True)
         self.recovery_sub = rospy.Subscriber("/move_base/recovery_status", RecoveryStatus, self.recovery_cb)
         self.move_base_sub = rospy.Subscriber("/move_base/result", MoveBaseActionResult, self.move_base_cb)
+        self.width_sub = rospy.Subscriber("/compute_y_distance/output", Float32, self.width_cb)
+        self.width = 0
 
     def speak(self, client, speech_text):
         client.say(speech_text, volume=1.0, replace=False)
@@ -32,9 +35,13 @@ class RestorePadding(object):
 
     def move_base_cb(self, msg):
         if self.shrink is True:
+            self.default_padding = max(self.width - 0.5, 0.1)
             self.client.update_configuration({"footprint_padding" : self.default_padding})
             rospy.loginfo("restore footprint_padding to {}".format(self.default_padding))
             self.shrink = False
+
+    def width_cb(self, msg):
+        self.width = msg.data
 
 if __name__ == '__main__':
     rospy.init_node('restore_padding')
